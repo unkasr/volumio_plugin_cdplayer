5bd0d7ec663846c4d4ea0af72d4835d8e1513d7f
macmpi <spam@ipik.org>
2017-12-11 14:27:39 +0100

    fix typo

--- plugins/music_service/cdplayer/install.sh
+++ plugins/music_service/cdplayer/install.sh
@@ -3,9 +3,9 @@
 DPKG_ARCH=`dpkg --print-architecture`
 
 if [ ${DPKG_ARCH} = "armhf" ]; then
-	LIB_GNUE="/lib/arm-linux-gnueabihf"
+	LIB_GNUE="/usr/lib/arm-linux-gnueabihf"
 elif [ ${DPKG_ARCH} = "i386" ]; then
-	LIB_GNUE="/lib/i386-linux-gnu"
+	LIB_GNUE="/usr/lib/i386-linux-gnu"
 fi
 
 echo "Installing cdplayer Dependencies"
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt-get -y install libdiscid0 eject
 
 # libdiscid package does not create all symlinks
-sudo ln -s /usr${LIB_GNUE}/libdiscid.so.0 /usr${LIB_GNUE}/libdiscid.so
+sudo ln -s ${LIB_GNUE}/libdiscid.so.0 ${LIB_GNUE}/libdiscid.so
 
 # make cd accessable for mpd
 sudo usermod -aG cdrom mpd
