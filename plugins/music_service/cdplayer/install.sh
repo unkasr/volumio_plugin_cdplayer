4b41beb38e8d6e7a853596489deabecff5b6bb5e
macmpi <spam@ipik.org>
2017-12-11 14:08:29 +0100

    fix install.sh for i386 & armhf install
    not fully tested

--- plugins/music_service/cdplayer/install.sh
+++ plugins/music_service/cdplayer/install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+DPKG_ARCH=`dpkg --print-architecture`
+
+if [ ${DPKG_ARCH} = "armhf" ]; then
+	LIB_GNUE="/lib/arm-linux-gnueabihf"
+elif [ ${DPKG_ARCH} = "i386" ]; then
+	LIB_GNUE="/lib/i386-linux-gnu"
+fi
+
 echo "Installing cdplayer Dependencies"
 sudo apt-get update
 # Install the required packages via apt-get
 sudo apt-get -y install libdiscid0 eject
 
 # libdiscid package does not create all symlinks
-sudo ln -s /usr/lib/arm-linux-gnueabihf/libdiscid.so.0 /usr/lib/arm-linux-gnueabihf/libdiscid.so
+sudo ln -s /usr${LIB_GNUE}/libdiscid.so.0 /usr${LIB_GNUE}/libdiscid.so
 
 # make cd accessable for mpd
 sudo usermod -aG cdrom mpd
-
-# If you need to differentiate install for armhf and i386 you can get the variable like this
-#DPKG_ARCH=`dpkg --print-architecture`
-# Then use it to differentiate your install
 
 #requred to end the plugin install
 echo "plugininstallend"
